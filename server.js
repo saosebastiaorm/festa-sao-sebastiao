e777979e5a7798543bbc30ee25273c8de0325fd2
saosebastiaorm <saosebastiaorm@gmail.com>
2026-05-09 00:47:14 -0400

    fix cors admin local

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:5500",
   "http://localhost:3000",
+  "http://127.0.0.1:3000",
+  "http://localhost:5501",
+  "http://127.0.0.1:5501",
+  "null",
   "https://festa-sao-sebastiao.vercel.app"
 ].filter(Boolean);
 
